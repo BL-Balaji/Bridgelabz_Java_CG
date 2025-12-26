a084c3e01b7f9b3488079be1d119ebeacb77483c
BL-Balaji <balaji.sapkal@bridgelabz.com>
2025-12-26 11:01:56 +0530

    UC3: Refactor banner logic into reusable methods

--- src/OopsBannerApp.java
+++ src/OopsBannerApp.java
@@ -1,17 +1,52 @@
 public class OopsBannerApp  {
-    public static void main(String[] args) {
-        String[] banner = {
-                " ***   ***  ****   *** ",
-                "*   * *   * *   * *   *",
-                "*   * *   * ****   *** ",
-                "*   * *   * *      *  ",
-                "*   * *   * *      *  ",
-                "*   * *   * *      *  ",
-                " ***   ***  *       ***"
+    public static String[] getOPattern()
+    {
+        return new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        };
+    }
+    public static String[] getPPattern()
+    {
+        return new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    ",
+                "*    ",
+                "*    "
+        };
+    }
+
+    public static String[] getSPattern()
+    {
+        return new String[]{
+                " **** ",
+                "*     ",
+                "*     ",
+                " **** ",
+                "     *",
+                "     *",
+                " **** "
         };
 
-        for (String line : banner) {
-            System.out.println(line);
+    }
+    public static void main(String[] args) {
+        String[] getOPattern = getOPattern();
+        String[] getPPattern = getPPattern();
+        String[] getSPattern = getSPattern();
+
+        for(int i =0;i<getOPattern.length;i++)
+        {
+            System.out.println(
+                    getOPattern[i]+" "+ getOPattern[i]+" "+getPPattern[i]+" "+getSPattern[i]
+            );
         }
     }
 }
