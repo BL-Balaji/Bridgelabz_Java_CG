51ee061bca555a11fc8573e3f250252e5c069a84
BL-Balaji <balaji.sapkal@bridgelabz.com>
2025-12-27 15:34:50 +0530

    UC1 : whether an Employee is Present or Absent using RANDOM attendance.

--- src/programmingContruct/day4/EmployeeWageComputation.java
+++ src/programmingContruct/day4/EmployeeWageComputation.java
@@ -2,8 +2,13 @@ package programmingContruct.day4;
 
 public class EmployeeWageComputation {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee\n" +
-                "Wage Computation\n" +
-                "Program");
+        // Generate random number: 0 or 1
+        int attendance = (int) (Math.random() * 2);
+        System.out.println(attendance);
+        if (attendance == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
