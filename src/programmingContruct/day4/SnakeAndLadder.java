342fa8e51bf479e9a98b5fde8286747d5e17c990
BL-Balaji <balaji.sapkal@bridgelabz.com>
2025-12-27 15:20:43 +0530

    START : print Snake and Ladder.

---
+++ src/programmingContruct/day4/SnakeAndLadder.java
@@ -0,0 +1,8 @@
+package programmingContruct.day4;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Snake and\n" +
+                "Ladder");
+    }
+}
